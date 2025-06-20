b69162c63c0fd5548f9e2bacd79693f4ba602688
lsa0828 <lsa0828@duksung.ac.kr>
2025-06-20 20:31:27 +0900

    feat: add product feature

---
+++ shooking/src/main/java/com/example/shooking/entity/Product.java
@@ -0,0 +1,26 @@
+package com.example.shooking.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "member_seq_gen")
+    @SequenceGenerator(
+            name = "member_seq_gen",
+            sequenceName = "member_seq",
+            allocationSize = 1
+    )
+    private Long id;
+    private String brand;
+    private String description;
+    private Long price;
+    @Column(name = "image_path")
+    private String imagePath;
+}
---
+++ shooking/src/main/java/com/example/shooking/service/ProductService.java
@@ -0,0 +1,41 @@
+package com.example.shooking.service;
+
+import com.example.shooking.dto.ImageData;
+import com.example.shooking.entity.Product;
+import com.example.shooking.repository.ProductRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class ProductService {
+    private final ProductRepository productRepository;
+
+    public List<Product> getAllProducts() {
+        return productRepository.findAll();
+    }
+
+    public ImageData getImage(Long id) throws IOException {
+        Product product = productRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("해당 상품이 존재하지 않습니다."));
+
+        File imageFile = new File("C:\\shooking\\ShookingShop\\shooking-shop\\public" + product.getImagePath());
+        if (!imageFile.exists()) {
+            throw new FileNotFoundException("이미지 파일이 존재하지 않습니다.");
+        }
+
+        byte[] data = Files.readAllBytes(imageFile.toPath());
+        String contentType = Files.probeContentType(imageFile.toPath());
+        if (contentType == null) {
+            contentType = "application/octet-stream";
+        }
+
+        return new ImageData(data, contentType);
+    }
+}
